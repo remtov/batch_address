ba415392eb3c5de2b4fee75df27f7b92cbfbe1da
koitt03-A <koitt03-A@koitt03-A-PC>
2018-09-05 12:59:50 +0900

    34343

--- batch_address/src/batch_address/AddrListDAOImpl.java
+++ batch_address/src/batch_address/AddrListDAOImpl.java
@@ -5,45 +5,40 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
+public class AddrListDAOImpl implements AddrListDAO {
 
+	public int insertAddList(List<List<String>> list) throws SQLException {
 
-public class AddrListDAOImpl implements AddrListDAO{
+		long s = System.currentTimeMillis();
+		Connection con = DBCon.getCon();
+		PreparedStatement ps = null;
+		int rCnt = 0;
+		String sql = "insert into addr_list values(seq_alnum.nextval,?,?,?,?,?,?,?,?,?,?,?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			for (int j = 0; j < list.size(); j++) {
+				List<String> strList = list.get(j);
+				for (int i = 0; i < strList.size(); i++) {
+					ps.setString(i + 1, strList.get(i));
 
-	public int insertAddList (List<List<String>> list) throws SQLException {
-		
-	long s = System.currentTimeMillis();
-	Connection con = DBCon.getCon();
-	PreparedStatement ps = null;
-	int rCnt = 0;
-	String sql= "insert into addr_list values(seq_alnum.nextval,?,?,?,?,?,?,?,?,?,?,?,?)";
-	try {
-		ps=con.prepareStatement(sql);
-		for(int j=0;j<list.size();j++) {
-			List<String> strList = list.get(j);
-			for(int i =0;i<strList.size();i++) {
-				ps.setString(i+1, strList.get(i));
-				
+				}
+				ps.addBatch();
+				ps.clearParameters();
+				if (j + 1 % 1000 == 0 || j + 1 == list.size()) {
+					rCnt += ps.executeBatch().length;
+				}
 			}
-			ps.addBatch();
-			ps.clearParameters();
-			if(j+1%1000==0 || j+1==list.size()) {
-				rCnt += ps.executeBatch().length;
-			}
+			DBCon.commit();
+		} catch (SQLException e) {
+			DBCon.rollback();
+			e.printStackTrace();
+		} finally {
+			DBCon.close();
+
 		}
-		DBCon.commit();
-	}catch(SQLException e) {
-		DBCon.rollback();
-		e.printStackTrace();
-	}finally{
-		DBCon.close();
-		
-			}
-	System.out.println("총 수행시간 : " +(System.currentTimeMillis()));
-	return rCnt;
-		
-		
+		System.out.println("총 수행시간 : " + (System.currentTimeMillis()));
+		return rCnt;
+
 	}
-	
-	
 
 }
---
+++ batch_address/src/batch_address/XmlPaser.java
@@ -0,0 +1,43 @@
+package batch_address;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XmlPaser {
+
+	public static void main(String[] args) {
+		String path = "/config/test.xml"; //스트링패스에 테스트의 경로를 넣는다
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance(); // 도큐멘트 생산하는 공장 dbf에 newInstance 하여 dbf의
+																			// 인스턴스를 만든다
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			InputStream is = XmlPaser.class.getResourceAsStream(path);
+			Document doc = db.parse(is);
+			NodeList foodList = doc.getElementsByTagName("foods");
+
+			for (int i = 0, max = foodList.getLength(); i < max; i++) {
+				Node food = foodList.item(i);
+				System.out.println(food.getTextContent());
+			}
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();//
+
+		}
+
+	}
+
+}
